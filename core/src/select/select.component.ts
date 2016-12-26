92a7d0fc9f8983eae9d0bb7568ade3065cb3d8e8
dhiegosad <dhiego.sad@afferolab.com.br>
2016-12-26 09:12:22 -0200

    update package.json, select.component

--- core/src/select/select.component.ts
+++ core/src/select/select.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter, OnChanges} from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnChanges, SimpleChanges} from '@angular/core';
 import { _ } from 'underscore';
 
 @Component({
