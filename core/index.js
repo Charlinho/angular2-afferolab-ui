e72325f54c802d850619aad3447dcbfa71f575d5
Charleston Campos <charleston.campos@afferolab.com.br>
2017-01-20 13:29:02 -0200

    Fix core components.

--- core/index.js
+++ core/index.js
@@ -1,3 +1,5 @@
 export { LoaderModule } from './src/loader/loader.module';
 export { SelectModule } from './src/select/select.module';
 export { ToastMessageModule } from './src/toast-message/toast-message.module';
+export { SemaphoreModule } from './src/semaphore/semaphore.module';
+export { TooltipModule } from './src/tooltip/tooltip.module';
